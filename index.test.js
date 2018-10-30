f08c717a85b94e10052f2e8dbc2da1b31b0a4c05
Kritsada <kritsitephuket@gmail.com>
2018-10-30 16:06:33 +0700

    Remove comment and new version 1.0.1

--- index.test.js
+++ index.test.js
@@ -6,11 +6,9 @@ test("Should Return 0 IF Amuont is 1 And Differance Bank", () => {
   const destination = 'SCB'
   const amount = 1
   const fee = 0
-
-  // Action 
+ 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 
@@ -19,10 +17,8 @@ test("Should Return 0 IF Amuont is 500 And Differance Bank", () => {
   const amount = 500
   const fee = 0
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 
@@ -31,10 +27,8 @@ test("Should Return 0 IF Amuont is 4999 And Differance Bank", () => {
   const amount = 4999
   const fee = 0
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 
@@ -42,11 +36,9 @@ test("Should Return 0 IF Amuont is 5000 And Differance Bank", () => {
   const destination = 'SCB'
   const amount = 5000
   const fee = 0
-
-  // Action 
+  
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 
@@ -55,10 +47,8 @@ test("Should Return 1 IF Amuont is 5001 And Differance Bank", () => {
   const amount = 5001
   const fee = 1
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 
@@ -67,10 +57,8 @@ test("Should Return 1 IF Amuont is 9999 And Differance Bank", () => {
   const amount = 9999
   const fee = 1
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 
@@ -79,10 +67,8 @@ test("Should Return 1 IF Amuont is 10000 And Differance Bank", () => {
   const amount = 10000
   const fee = 1
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 
@@ -91,10 +77,8 @@ test("Should Return 2 IF Amuont is 10001 And Differance Bank", () => {
   const amount = 10001
   const fee = 2
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 
@@ -103,10 +87,8 @@ test("Should Return 2 IF Amuont is 14999 And Differance Bank", () => {
   const amount = 14999
   const fee = 2
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 
@@ -115,10 +97,8 @@ test("Should Return 2 IF Amuont is 15000 And Differance Bank", () => {
   const amount = 15000
   const fee = 2
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 
@@ -127,10 +107,8 @@ test("Should Return 3 IF Amuont is 15001 And Differance Bank", () => {
   const amount = 15001
   const fee = 3
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 test("Should Return 3 IF Amuont is 19999 And Differance Bank", () => {
@@ -138,10 +116,8 @@ test("Should Return 3 IF Amuont is 19999 And Differance Bank", () => {
   const amount = 19999
   const fee = 3
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 test("Should Return 3 IF Amuont is 20000 And Differance Bank", () => {
@@ -149,9 +125,8 @@ test("Should Return 3 IF Amuont is 20000 And Differance Bank", () => {
   const amount = 20000
   const fee = 3
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
-  //Ass
+
   expect(result).toBe(fee)
 })
 
@@ -160,10 +135,8 @@ test("Should Return 0 IF Amuont is 5000 And Same Bank", () => {
   const amount = 5000
   const fee = 0
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 
@@ -172,10 +145,8 @@ test("Should Return 0 IF Amuont is 10000 And Same Bank", () => {
   const amount = 10000
   const fee = 0
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 
@@ -184,10 +155,8 @@ test("Should Return 0 IF Amuont is 15000 And Same Bank", () => {
   const amount = 15000
   const fee = 0
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 
@@ -196,10 +165,8 @@ test("Should Return 0 IF Amuont is 20000 And Same Bank", () => {
   const amount = 20000
   const fee = 0
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 
@@ -208,10 +175,8 @@ test("Should Return NULL IF Amuont is -1", () => {
   const amount = -1
   const fee = null
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 test("Should Return NULL IF Amuont is 0", () => {
@@ -219,10 +184,8 @@ test("Should Return NULL IF Amuont is 0", () => {
   const amount = 0
   const fee = null
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 test("Should Return NULL IF Amuont is 20001", () => {
@@ -230,32 +193,18 @@ test("Should Return NULL IF Amuont is 20001", () => {
   const amount = 20001
   const fee = null
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
-test("Should Return NULL IF Amuont is 1000.5", () => {
+
+test("Should Return NULL IF Amuont is 1000.5 *", () => {
   const destination = 'KBANK'
   const amount = 1000.5
   const fee = null
 
-  // Action 
   const result = index.calculateFee(source, destination, amount)
 
-  //Ass
   expect(result).toBe(fee)
 })
 
-// test("Should Return NULL IF Amuont is 1000.5 (string)", () => {
-//   const destination = 'KBANK'
-//   const amount = '1000.5'
-//   const fee = null
-
-//   // Action 
-//   const result = index.calculateFee(source, destination, amount)
-
-//   //Ass
-//   expect(result).toBe(fee)
-// })
